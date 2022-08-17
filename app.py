a03d2635760cced2ae65a500cab700d2f57fdbc5
Purnima Venkatram <purnima.venkatram@gmail.com>
2022-08-17 21:24:07 +0000

    Changes to use olympic medals dataset

--- app.py
+++ app.py
@@ -7,19 +7,17 @@ import pandas as pd
 
 ########### Define your variables ######
 
-tabtitle = 'Old McDonald'
+tabtitle = 'Olympic Medals'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/agriculture-exports-map'
+githublink = 'https://github.com/purnimavenkatram/306-agriculture-exports-dropdown'
 # here's the list of possible columns to choose from.
-list_of_columns =['total exports', 'beef', 'pork', 'poultry',
-       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+list_of_columns =['summer_participations','summer_gold','summer_silver','summer_bronze','summer_total','winter_gold','winter_silver','winter_total']
 
 
 ########## Set up the chart
 
 import pandas as pd
-df = pd.read_csv('assets/usa-2011-agriculture.csv')
+df = pd.read_csv('assets/olympics_medals_country_wise.csv')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -30,14 +28,14 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1('2011 Agricultural Exports, by State'),
+    html.H1('Olympic Medals Country Wise since 1896'),
     html.Div([
         html.Div([
                 html.H6('Select a variable for analysis:'),
                 dcc.Dropdown(
                     id='options-drop',
                     options=[{'label': i, 'value': i} for i in list_of_columns],
-                    value='corn'
+                    value='summer_total'
                 ),
         ], className='two columns'),
         html.Div([dcc.Graph(id='figure-1'),
@@ -54,13 +52,13 @@ app.layout = html.Div(children=[
 @app.callback(Output('figure-1', 'figure'),
              [Input('options-drop', 'value')])
 def make_figure(varname):
-    mygraphtitle = f'Exports of {varname} in 2011'
+    mygraphtitle = f' {varname} Medals since 1896'
     mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-    mycolorbartitle = "Millions USD"
+    mycolorbartitle = "Count"
 
     data=go.Choropleth(
-        locations=df['code'], # Spatial coordinates
-        locationmode = 'USA-states', # set of locations match entries in `locations`
+        locations=df['ioc-code'], # Spatial coordinates
+        locationmode = 'country names', # set of locations match entries in `locations`
         z = df[varname].astype(float), # Data to be color-coded
         colorscale = mycolorscale,
         colorbar_title = mycolorbartitle,
@@ -68,7 +66,7 @@ def make_figure(varname):
     fig = go.Figure(data)
     fig.update_layout(
         title_text = mygraphtitle,
-        geo_scope='usa',
+        geo_scope='world',
         width=1200,
         height=800
     )
